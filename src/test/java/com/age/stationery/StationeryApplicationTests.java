79884bb93f9a1d10a85b7206fb06d397a7ddcd96
Night-human <agexandro@gmail.com>
2025-06-12 14:55:52 -0600

    Added initial folder structure

---
+++ src/test/java/com/age/stationery/StationeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.age.stationery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
